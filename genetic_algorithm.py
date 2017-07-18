a10290892bf9c879ac5cb4e5437e2881d0598ce5
jgmn <jesus.gerardo.mn@outlook.com>
2017-07-18 12:32:49 +0200

    Ajuste en los operadores

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -3,31 +3,37 @@ import random
 import geopandas as gpd
 from deap import creator, base, tools, algorithms
 
+# Creación de tipos
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
 
-# Obtener el tamaño del cromosoma
-cali = gpd.read_file('calificaciones_filtrado.JSON') 
-ind_size = len(cali)
+#Tamaño del cromosoma
+ind_size = 1
 
-toolbox = base.Toolbox()
-toolbox.register("attr_bool", random.randint, 0, 1)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, n=ind_size)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+# Obtener el tamaño de la población
+cali = gpd.read_file('calificaciones_filtrado.JSON') 
+pop_size = len(cali) 
 
 # Función de Fitness
-def evaluateModel(individual):
+def evalFitness(individual):
     return sum(individual),
 
-toolbox.register("evaluate", evaluateModel)
-toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
-toolbox.register("select", tools.selTournament, tournsize=3)
+# Inicialización
+toolbox = base.Toolbox()
+toolbox.register("bit", random.randint, 0, 1)
+toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.bit, ind_size)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual, pop_size)
 
-ngen = 100
-pop_size = 1
-pop = toolbox.population(pop_size)
-res = algorithms.eaSimple(pop, toolbox, cxpb = 0.5, mutpb = 0.1, ngen = ngen, verbose = False) #http://deap.readthedocs.io/en/1.0.x/api/algo.html#complete-algorithms
+# Operadores
+toolbox.register("evaluate", evalFitness)
+toolbox.register("mate", tools.cxUniform, indpb=0.5) # http://deap.readthedocs.io/en/master/api/tools.html#operators
+toolbox.register("mutate", tools.mutFlipBit, indpb = 0.05)
+toolbox.register("select", tools.selBest)
+
+# Algoritmo Genético 
+pop = toolbox.population()
+cxpb, mutpb, ngen = 0.5, 0.1, 100
+res = algorithms.eaSimple(pop, toolbox, cxpb, mutpb, ngen, verbose = False) #http://deap.readthedocs.io/en/1.0.x/api/algo.html#complete-algorithms
 
 print('---POBLACIÓN FINAL---')
 print(pop)
