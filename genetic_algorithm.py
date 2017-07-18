3195e3f5f82c14687e8f20f2034ec404d9638375
jgmn <jesus.gerardo.mn@outlook.com>
2017-07-18 15:34:38 +0200

    Cambio en los operadores

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -21,18 +21,18 @@ def evalFitness(individual):
 # Inicialización
 toolbox = base.Toolbox()
 toolbox.register("bit", random.randint, 0, 1)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.bit, ind_size)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual, pop_size)
+toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.bit, pop_size)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual, ind_size)
 
 # Operadores
 toolbox.register("evaluate", evalFitness)
-toolbox.register("mate", tools.cxUniform, indpb=0.5) # http://deap.readthedocs.io/en/master/api/tools.html#operators
+toolbox.register("mate", tools.cxTwoPoint, indpb=0.5) #http://deap.readthedocs.io/en/master/api/tools.html#operators
 toolbox.register("mutate", tools.mutFlipBit, indpb = 0.05)
 toolbox.register("select", tools.selBest)
 
 # Algoritmo Genético 
 pop = toolbox.population()
-cxpb, mutpb, ngen = 0.5, 0.1, 100
+cxpb, mutpb, ngen = 0.5, 0.1, 50
 res = algorithms.eaSimple(pop, toolbox, cxpb, mutpb, ngen, verbose = False) #http://deap.readthedocs.io/en/1.0.x/api/algo.html#complete-algorithms
 
 print('---POBLACIÓN FINAL---')
