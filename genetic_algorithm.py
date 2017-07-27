3165803306d5bb09271d673e5868e96ad7dbb095
jgmn <jesus.gerardo.mn@outlook.com>
2017-07-27 16:21:04 +0200

    Cambio en el nombre de variables

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -7,32 +7,37 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 from deap import creator, base, tools, algorithms
 
-def execute_genetic_algorithm(ind_size, pop_size, cali_df, voro_df):
+def execute_genetic_algorithm(ind_size, pop_size, pdi_df, voro_df):
     # Creación de tipos
     creator.create("FitnessMax", base.Fitness, weights = (1.0,))
     creator.create("Individual", list, fitness = creator.FitnessMax)
     
+    # Cambiar el formato de coordendas para el cálculo de áreas
+    voro_df = voro_df.to_crs({'init': 'epsg:25830'}) # http://spatialreference.org/ref/epsg/25830/proj4js/
+    
     # Precálculo de totales 
-    pob_total = sum(cali_df['poblacion'])
-    trafico_total = sum(cali_df['trafico'])
-    tweets_total = sum(cali_df['tweets'])
-    area_total = sum(voro_df['geometry'].area)
+    pob_total = sum(pdi_df['poblacion'])
+    trafico_total = sum(pdi_df['trafico'])
+    tweets_total = sum(pdi_df['tweets'])
+    tiempo_total = sum(pdi_df['tiempo'])
+    area_total = sum(voro_df['geometry'].area) #El área es en m^2
     
     # Función de fitness
     def evalFitness(individual):
+        utilidad, poblacion, trafico, tweets, tiempo = 0, 0, 0, 0, 0
         poblacion, trafico, tweets, tiempo, area = 0, 0, 0, 0, 0
         utilidad, coste, coste_unitario = 0, 0, 1 
-        num_estaciones = sum(individual)
+        num_estaciones = sum(individual) / ind_size
         for index, elem in enumerate(individual):
             if(elem == 1):
-                poblacion += cali_df['poblacion'][index] / pob_total
-                trafico += cali_df['trafico'][index] / trafico_total
-                tweets += cali_df['tweets'][index]  / tweets_total
-                tiempo += cali_df['tiempo_medio'][index]
-                area += voro_df['geometry'][index].area / area_total  
-                coste += area + coste_unitario * num_estaciones
-                utilidad += poblacion + trafico + tiempo + tweets
-                utilidad = utilidad - coste                
+                poblacion += pdi_df['poblacion'][index] / pob_total
+                trafico += pdi_df['trafico'][index] / trafico_total
+                tweets += pdi_df['tweets'][index] / tweets_total
+                tiempo += pdi_df['tiempo'][index] / tiempo_total
+                area += voro_df['geometry'][index].area / area_total
+        utilidad += poblacion + trafico + tiempo + tweets 
+        coste = area + coste_unitario * num_estaciones  
+        utilidad = utilidad - coste              
         return utilidad,
     
     # Inicialización
@@ -43,37 +48,33 @@ def execute_genetic_algorithm(ind_size, pop_size, cali_df, voro_df):
     
     # Operadores
     toolbox.register("evaluate", evalFitness)
-    toolbox.register("mate", tools.cxOnePoint)
+    toolbox.register("mate", tools.cxTwoPoint)
     toolbox.register("mutate", tools.mutFlipBit, indpb = 0.05)
     toolbox.register("select", tools.selTournament, tournsize = 3)
     
     # Algoritmo genético
     pop = toolbox.population()
+    hof = tools.HallOfFame(1)
     stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("min", numpy.min)
-    stats.register("avg", numpy.mean)
     stats.register("max", numpy.max)
-    pop, logbook = algorithms.eaSimple(pop, toolbox, cxpb = 0.5, mutpb = 0.2, ngen = 100, stats = stats, verbose = False)
+    pop, logbook = algorithms.eaSimple(pop, toolbox, cxpb = 0.5, mutpb = 0.2, ngen = 100, stats = stats, halloffame = hof, verbose = False)
 
-    return pop, logbook
+    return pop, logbook, hof
 
-def save_charging_stations(best, cali, date):
-    # Filtrado de población
+def save_charging_stations(best, pdi, date):
     result = {}
-    result['type'] = cali['type']
-    result['crs'] = cali['crs']
+    result['type'] = pdi['type']
+    result['crs'] = pdi['crs']
     result['features'] = []
     
     for index, elem in enumerate(best[0]):
         if(elem == 1):
-            
-            feature = cali['features'][index]
+            feature = pdi['features'][index]
             result['features'].append(feature)        
     
     result['crs']['properties']['name'] = "urn:ogc:def:crs:EPSG::4326"
     
-    # Escribir el archivo de salida
-    with open("estaciones_de_recarga_"+date+".JSON", "w") as output_file:
+    with open("estaciones_de_recarga_"+date+".json", "w") as output_file:
         json.dump((result), output_file, indent = 3)
         
 def save_logbook(logbook, date):   
@@ -86,24 +87,17 @@ def save_logbook(logbook, date):
 def save_graph(logbook, date): 
     gen = logbook.select("gen")
     fit_max = logbook.select("max")
-    fit_avg = logbook.select("avg")
         
     fig, ax1 = plt.subplots()
-    line1 = ax1.plot(gen, fit_max, "b-", label = "Maximum Fitness")
-    ax1.set_xlabel("Generation")
-    ax1.set_ylabel("Max Fitness", color="b")
+    line1 = ax1.plot(gen, fit_max, "b-", label = "Función Objetivo")
+    ax1.set_xlabel("Generaciones")
+    ax1.set_ylabel("Función Objetivo", color="b")
     for tl in ax1.get_yticklabels():
         tl.set_color("b")
-    
-    ax2 = ax1.twinx()
-    line2 = ax2.plot(gen, fit_avg, "r-", label = "Average Fitness")
-    ax2.set_ylabel("Avg Fitness", color="r")
-    for tl in ax2.get_yticklabels():
-        tl.set_color("r")
-    
-    lns = line1 + line2
+        
+    lns = line1
     labs = [l.get_label() for l in lns]
-    ax1.legend(lns, labs, loc = None)
+    ax1.legend(lns, labs)
     
     fig.savefig("grafica_fitness_"+date+".PNG")
         
@@ -114,19 +108,18 @@ def main():
     date = date.replace(".", "")
     date = date.replace(" ", "")
     
-    with open('calificaciones_filtrado.JSON', 'r') as input_file:
-        cali = json.load(input_file)
+    with open('puntos_de_interes.json', 'r') as input_file:
+        pdi = json.load(input_file)
     
-    cali_df = gpd.read_file('calificaciones_filtrado.JSON')
-    voro_df = gpd.read_file('voronoi.JSON')
+    pdi_df = gpd.read_file('puntos_de_interes.json')
+    voro_df = gpd.read_file('voronoi.json')
     
     # Tamaño del cromosoma y población
-    ind_size = len(cali_df['geometry'])
-    pop_size = 80
+    ind_size = max(pdi_df.index) + 1
+    pop_size = 300
     
-    pop, logbook = execute_genetic_algorithm(ind_size, pop_size, cali_df, voro_df)    
-    best = tools.selBest(pop, k = 1)
-    save_charging_stations(best, cali, date)
+    pop, logbook, best = execute_genetic_algorithm(ind_size, pop_size, pdi_df, voro_df)    
+    save_charging_stations(best, pdi, date)
     save_logbook(logbook, date)
     save_graph(logbook, date)
         
